4d71d934de2d44d3b5c852c7f6e90f8d5f4aaf19
pwrtagServer <mm.luqman53@gmail.com>
2022-09-25 04:44:03 +0000

    test and fix meta:data for welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,12 @@
     <head>
     <meta charset="utf-8">
     <meta property="og:title" content="Pwrtag" />
-    <meta property="og:type" content="video.movie" />
-    <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-    <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+    <meta property="og:description" 
+    content="Show us that your biceps is worthy." />
+    <meta property="og:description " content="Show us that your biceps is worthy" />
+    <!-- <meta property="og:type" content="video.movie" /> -->
+    <meta property="og:url" content="https://pwrtag.com/" />
+    <meta property="og:image" content="{{ url('storage/Pwrtag_Neon.PNG') }}" />
     <title>{{env("APP_NAME")}}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
